063de6ec2c49ac51d760b70bee89bdc2ffa2b85b
Suhail <mohammedsuhail100506@gmail.com>
2026-01-30 21:36:36 +0530

    feat: Add JobListing component with search and filter functionality and switch API endpoint to localhost.

--- src/Components/JobListing.jsx
+++ src/Components/JobListing.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import JobCard from '../components/JobCard';
+import JobCard from './JobCard';
 import API from '../utils/api';
 
 const JobListing = () => {
